69fba2663bb0bb827b163417e80895ad8c19053f
William Young <Wcyoun03@louisville.edu>
2022-11-08 20:06:57 -0500

    My solution for lesson 1

---
+++ Lessons/Lesson 1/Solutions/lesson1WCY.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Please enter a number: ");
+double x; //declalres an unitialized integer x
+char op;
+double y; 
+x = Convert.ToDouble(Console.ReadLine()); //reads in an integer and stores it in x
+op = Convert.ToChar(Console.ReadLine());
+y = Convert.ToDouble(Console.ReadLine());
+switch(op)
+{
+    case '%': 
+            Console.WriteLine(x + " % " + y + " = " + (x%y));
+        break;
+    case '*':
+            Console.WriteLine(x + " * " + y + " = " + (x*y));
+        break;
+    case '/':
+            Console.WriteLine(x + " / " + y + " = " + (x/y));
+        break;
+    case '-':
+            Console.WriteLine(x + " - " + y + " = " + (x-y));
+        break;
+    case '+':
+            Console.WriteLine(x + " + " + y + " = " + (x+y));
+        break;
+}
+
+
+
+Console.WriteLine(x); //outputs x
