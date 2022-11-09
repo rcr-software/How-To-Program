ae051f976b3d714006c26f7b0c269f5aec363f03
Seth Gibbs <sngibb02@louisville.edu>
2022-11-08 19:08:00 -0500

    My lesson 1 solution

---
+++ Lessons/Lesson 1/Solutions/SG.cs
@@ -0,0 +1,23 @@
+﻿
+int x;
+char sign;
+int y;
+
+Console.Write("What is the first number?");
+x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the operator:");
+sign = Convert.ToChar(Console.ReadLine());
+Console.Write("What is the last number?");
+y = Convert.ToInt32(Console.ReadLine());
+
+if (sign == '+')
+    Console.WriteLine(x + y);
+else if (sign == '-')
+    Console.WriteLine(x - y);
+else if (sign == '*')
+    Console.WriteLine(x * y);
+else if (sign == '/')
+    Console.WriteLine(x / y);
+else if (sign == '%')
+    Console.WriteLine(x % y);
+
